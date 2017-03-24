3b5295d5d451f366e0a268e81594c1c28814d785
roberto.jwsilva <betowithowsky@gmail.com>
2017-03-23 22:12:01 -0300

    Aulas Quinta - Professor Ramon

--- src/aulasjavaquinta/CalculoBhaskara.java
+++ src/aulasjavaquinta/CalculoBhaskara.java
@@ -29,8 +29,8 @@ public class CalculoBhaskara {
         
         //PROCESSAMENTO
         double delta = (b*b) - ((4 * a) * c);
-        double x1 = -b + Math.sqrt(delta) / 2 * a;
-        double x2 = -b - Math.sqrt(delta) / 2 * a;
+        double x1 = (-b + Math.sqrt(delta)) / (2 * a);
+        double x2 = (-b - Math.sqrt(delta)) / (2 * a);
         
         
         //SAIDA
