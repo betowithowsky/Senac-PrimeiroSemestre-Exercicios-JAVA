a203810cd6f5472545022719c479f2073657783b
roberto.jwsilva <roberto.jwsilva@CAS555596W0750.senacsp.edu.br>
2017-04-27 19:50:22 -0300

    jogo da velha completo

---
+++ src/aulasjavaquinta/JogoDaVelhaCompleto.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aulasjavaquinta;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author roberto.jwsilva
+ */
+public class JogoDaVelhaCompleto {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        String player;
+        
+        String pos1 = "1";
+        String pos2 = "2";
+        String pos3 = "3";
+        String pos4 = "4";
+        String pos5 = "5";
+        String pos6 = "6";
+        String pos7 = "7";
+        String pos8 = "8";
+        String pos9 = "9";
+        
+        System.out.println(" 1 | 2 | 3 ");
+        System.out.println("---+---+---");
+        System.out.println(" 4 | 5 | 6 ");
+        System.out.println("---+---+---");
+        System.out.println(" 7 | 8 | 9 ");
+        
+        System.out.println("Qual a Posição do X?");
+        
+    }
+}
