1088bc8f7112c40cb6f765fa8b7283ae326a8b1b
roberto.jwsilva <roberto.jwsilva@CAS511892W0716.senacsp.edu.br>
2017-08-14 19:27:39 -0300

    Segundo Semestre

---
+++ Algoritimos2TADS/src/algoritimos2tads/Algoritimos2Exercicio01.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algoritimos2tads;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author roberto.jwsilva
+ */
+public class Algoritimos2Exercicio01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Digite um Numero: ");
+        int num = input.nextInt();
+        
+        System.out.println(ParOuImpar(num));
+    }
+    
+    public static boolean ParOuImpar(int num){
+        boolean ParImpar = false;
+       if(num % 2 == 0){
+           ParImpar = true;
+       }
+       return ParImpar;  
+}
+}
